f75de4b9f3c91f972b9b65243ac850cf048dc842
Jab <drjabeennaz@gmail.com>
2026-07-17 00:39:21 +0530

    Restore clean production app bundle

--- outputs/src/app.js
+++ outputs/src/app.js
@@ -1,6 +1,3 @@
-Exit code: 0
-Wall time: 2.1 seconds
-Output:
 import { BADGES, LEVELS, MISSIONS, getLevel, getMission } from './data/missions.js';
 import { clearProgress, completionCount, currentMissionId, getLevelProgress, hasCompleted, isMissionOpen, loadState, levelRewardKey, saveState, totalXp } from './state.js';
 import { authApi, authMessage, familyApi, supabaseEnabled } from './integrations/supabase-service.js';
